384282c027260f4be6d9adc2f53b4dd066ef0b97
Daniel8bits <daniel.sftm@outlook.com>
2022-07-13 02:27:49 -0300

    finished home

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -4,6 +4,7 @@ import Column from '@layouts/grid/Column';
 import Row from '@layouts/grid/Row';
 import MainLayout from '@layouts/mainLayout/MainLayout';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 interface HomeProps {
   
@@ -19,15 +20,40 @@ const Home: React.FC<HomeProps> = () => {
           </Column>
         </Row>
         <Row>
-          <Column sm={4} lg={4} xl={6}> <h1> Daniel Noguês de Oliveira </h1> </Column>
-          <Column sm={4} lg={4} xl={6}> 
+          <Column sm={4} lg={4} xl={7}> 
+            <h1> Daniel Noguês de Oliveira </h1> 
+            <br  />
+            <p>
+              I&apos;m a guy who likes to reinvent the wheel and rediscover the fire
+              just to understand how things work <i> and definitely not because I always forget
+              to check if the library already exists ;~; </i>
+            </p>
+            <p>
+              I&apos;m 23 years old, brasilian 🇧🇷, currently studying Computer Science at <a href='https://portal.ufpel.edu.br/'> Federal University of Pelotas (UFPel) </a>
+              and have tons of experience in programming got from some private projects.
+            </p>
+            <p>
+              The technologies I know are: C, C++, Java, Javascript, NodeJS, PostgreSQL, 
+              OpenGL, GLSL, Blender and how to use a coffee machine which, everyone knows, is the purpose of a CS degree.
+            </p>
+          </Column>
+          <Column sm={4} lg={4} xl={5}> 
             <Avatar  /> 
           </Column>
         </Row>
         <Row>
-          <Column xl={4}> 3 </Column>
-          <Column xl={4}> 4 </Column>
-          <Column xl={4}> 5 </Column>
+          <Column>
+            <h2> Contacts </h2>
+          </Column>
+        </Row>
+        <Row>
+          <Column>
+            <p>
+              <b>Phone: </b> +55 (53) 999471840
+              <br />
+              <b>Email: </b> daniel.sftm@outlook.com
+            </p>
+          </Column>
         </Row>
       </ContentPanel>
     </MainLayout>
